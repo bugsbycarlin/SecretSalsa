28389c7be1246fee9c652b6635df6ada65bcd6fe
Matthew Carlin <bugsby.carlin@gmail.com>
2019-03-04 21:13:01 -0800

    Shuffle around projects, start drawing forest map, add skeleton for world builder tool

---
+++ 05_CoreRestaurant/Source/kitchen.cpp
@@ -0,0 +1,25 @@
+/*
+  Secret Salsa
+  Matthew Carlin
+  Copyright 2019
+*/
+
+#include "kitchen.h"
+
+using namespace std;
+using namespace Honey;
+
+Kitchen::Kitchen() {
+}
+
+void Kitchen::initialize() {
+}
+
+void Kitchen::logic() {
+}
+
+void Kitchen::render() {
+}
+
+Kitchen::~Kitchen() {
+}
